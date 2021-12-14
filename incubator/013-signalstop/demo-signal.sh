2533d61978433c3dfb95921d5f28899b84f64e6d
Alejandro <alejandro.visiedo@gmail.com>
2021-12-14 20:50:52 +0100

    Improve exit message and remove duplicated code
    Add @tiran suggestion for redirecting exit message to stderr and remove initial duplicated code.

--- incubator/013-signalstop/demo-signal.sh
+++ incubator/013-signalstop/demo-signal.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-function trap_sigint { echo "Exiting by SIGINT";  exit 0; }
-function trap_sigterm { echo "Exiting by SIGTERM"; exit 0; }
-function trap_sigusr1 { echo "Exiting by SIGUSR1"; exit 0; }
-function trap_sigrtmin3 { echo "Exiting by SIGRTMIN+3"; exit 0; }
+function trap_signal {
+  local signal="$1"
+  echo -e "\nExiting by ${signal}" >&2
+  exit 0
+}
 
-trap trap_sigint SIGINT
-trap trap_sigterm SIGTERM
-trap trap_sigusr1 SIGUSR1
-trap trap_sigrtmin3 "SIGRTMIN+3"
+for signal in SIGINT SIGTERM SIGUSR1 "SIGRTMIN+3"
+do
+  trap "trap_signal '${signal}'" "${signal}"
+done
 
 while true; do
     echo -n "."
